e52fac79dfea4733a9df5821dc88d6024e1a047f
adrianchange <adrianchange@gmail.com>
2022-01-09 16:21:30 +0000

    Commit on main

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +6,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  let randomSuitsNumber = Math.floor(Math.random() * 4);
+  let number = Math.floor(Math.random() * (13 - 1) + 1);
+  let randomSuits = suits[randomSuitsNumber];
+  const card = document.querySelector(".card");
+
+  if (number == 10) {
+    number = "J";
+  } else if (number == 11) {
+    number = "Q";
+  } else if (number == 12) {
+    number = "K";
+  }
+  card.innerHTML = `
+                        <div class="${randomSuits} col-12 text-start">${randomSuits}</div>
+                        <div class="${number} col-12-auto text-center">${number}</div>
+                        <div class="${randomSuits} col-12 text-end">${randomSuits}</div>
+                     `;
 };
